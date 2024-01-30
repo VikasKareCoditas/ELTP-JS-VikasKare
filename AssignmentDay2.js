98bbeb3012acb6eae98b6b9f390811e6a2e5b43e
vikas.kare-coditas <158145117+VikasKareCoditas@users.noreply.github.com>
2024-01-30 16:27:51 +0530

    Assignment Day 2

---
+++ AssignmentDay2.js
@@ -0,0 +1,43 @@
+// // Declare four variables without assigning values
+
+const movieName;
+const movieBudget;
+const movieTime;
+let actorName;
+let actreesName;
+let directorsName;
+
+
+// // Declare four variables with assigned values
+
+const theaterName = "City Pride"; // string
+const sitingCapacity = 200; // int
+let currentScreens = 5; // int
+let isOpen = true; // boolean
+let dailyProfit = 1200.20; // double
+
+
+// // Declare variables to store your first name, last name, marital status, country and age in multiple lines
+
+const firstName = 'Vikas';
+const lastName = 'Kare';
+let isMarried = false;
+let country = "India";
+const age = 22;
+
+
+// // Declare variables to store your first name, last name, marital status, country and age in a single line
+
+const firstName = "Vikas", lastName = "Kare";
+let country = "India", isMarried = false;
+
+
+// // Declare two variables myAge and yourAge and assign them initial values and log to the browser console.
+
+const myAge = 22;
+console.log(`My Age : ${myAge}`);
+const yourAge = 18;
+console.log(`Your Age : ${yourAge}`);
+
+
+
